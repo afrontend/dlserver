ea02d08710d00166a6ce52d30a705dc3d2d5dc2c
afrontend <frontend.wordpress@gmail.com>
2017-09-18 21:31:51 +0900

    (new) 책 제목과 도서관 이름을 전달하면 책 제목과 대출 정보 리스트를 돌려줌

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const dl = require('dongnelibrary');
+
+app.get('/search', function (req, res, next) {
+  var title = '';
+  var libraryName = '';
+
+  if (req.query.title) {
+    title = req.query.title
+  }
+
+  if (req.query.libraryName) {
+    libraryName = req.query.libraryName
+  }
+
+  dl.search({
+    title: title,
+    libraryName: libraryName
+  }, null, function (err, books) {
+    if(err) {
+      res.json({
+        message: err.msg
+      });
+    } else {
+      res.json(books);
+    }
+  });
+})
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: 'Server Error'
+  });
+});
+
+app.listen(3000, function () {
+  console.log('dlserver listening on port 3000!')
+})
