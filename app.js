03203294d3a8e32c0e3782a7b6b7bf25a025d1ab
afrontend <autoscripts@gmail.com>
2026-01-17 23:41:42 +0900

    Refactor app.js to use modern JavaScript patterns
    Replace callback-based async with async/await using a promisified
    wrapper for dl.search. Add extractSearchParams helper to reduce
    duplication and modernize syntax (const, arrow functions, template
    literals).

--- app.js
+++ app.js
@@ -1,91 +1,81 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dl = require('dongnelibrary');
+const dl = require("dongnelibrary");
 
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 
-function makeBookDescription(book) {
-  var mark = book.exist ? '✓ ' : '✖ ';
-  return " "+mark+" " + book.title + "<br>";
-}
-
-app.get('/:title/:libraryName', function (req, res, next) {
-  var title = '';
-  var libraryName = '';
-
-  if (req.params.title) {
-    title = req.params.title
-  }
-
-  if (req.params.libraryName) {
-    libraryName = req.params.libraryName
-  }
-
-  dl.search({
-    title: title,
-    libraryName: libraryName
-  }, null, function (err, books) {
-    if(err) {
-      res.json({
-        message: err.msg
-      });
-    } else {
-      res.send(books[0].booklist.reduce(function (memo, book) {
-        return memo + makeBookDescription(book);
-      }, ''));
-    }
-  });
-})
-
-app.get('/search', function (req, res, next) {
-  var title = '';
-  var libraryName = '';
-
-  if (req.query.title) {
-    title = req.query.title
-  }
-
-  if (req.query.libraryName) {
-    libraryName = req.query.libraryName
-  }
-
-  if (title === '' && libraryName === '') {
-    var libs = dl.getLibraryNames();
-    res.send(libs.join("<br>"));
-  } else {
-    dl.search({
-      title: title,
-      libraryName: libraryName
-    }, null, function (err, books) {
-      if(err) {
-        res.json({
-          message: err.msg
-        });
+// Promisified wrapper for dl.search
+const searchBooks = (title, libraryName) => {
+  return new Promise((resolve, reject) => {
+    dl.search({ title, libraryName }, null, (err, books) => {
+      if (err) {
+        reject(err);
       } else {
-        res.json(books);
+        resolve(books);
       }
     });
+  });
+};
+
+// Extract search params from request params or query
+const extractSearchParams = (params) => ({
+  title: params.title || "",
+  libraryName: params.libraryName || "",
+});
+
+const makeBookDescription = (book) => {
+  const mark = book.exist ? "✓ " : "✖ ";
+  return " " + mark + " " + book.title + "<br>";
+};
+
+app.get("/:title/:libraryName", async (req, res) => {
+  const { title, libraryName } = extractSearchParams(req.params);
+
+  try {
+    const books = await searchBooks(title, libraryName);
+    res.send(
+      books[0].booklist.reduce((memo, book) => {
+        return memo + makeBookDescription(book);
+      }, ""),
+    );
+  } catch (err) {
+    res.json({ message: err.msg });
   }
-})
+});
 
-app.get('/libraryList', function (req, res, next) {
-  var libs = dl.getLibraryNames();
+app.get("/search", async (req, res) => {
+  const { title, libraryName } = extractSearchParams(req.query);
+
+  if (title === "" && libraryName === "") {
+    const libs = dl.getLibraryNames();
+    res.send(libs.join("<br>"));
+  } else {
+    try {
+      const books = await searchBooks(title, libraryName);
+      res.json(books);
+    } catch (err) {
+      res.json({ message: err.msg });
+    }
+  }
+});
+
+app.get("/libraryList", (req, res) => {
+  const libs = dl.getLibraryNames();
   res.json(libs);
-})
+});
 
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.json({
-    message: 'Server Error'
-  });
+  res.json({ message: "Server Error" });
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('dlserver listening on port 3000!')
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`dlserver listening on port ${port}!`);
+});
